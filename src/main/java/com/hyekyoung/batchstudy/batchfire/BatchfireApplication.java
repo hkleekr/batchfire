8fc52a1fb7a5702c146cd41a75ae00027067ced0
dkbmc_hyekyounglee <hyekyoung.lee@dkbmc.com>
2024-09-10 17:34:18 +0900

    project init

---
+++ src/main/java/com/hyekyoung/batchstudy/batchfire/BatchfireApplication.java
@@ -0,0 +1,13 @@
+package com.hyekyoung.batchstudy.batchfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchfireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchfireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyekyoung/batchstudy/batchfire/BatchfireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyekyoung.batchstudy.batchfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchfireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
